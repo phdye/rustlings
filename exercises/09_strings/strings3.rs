855958a6b195ecdf9fa7fcc1151cf881d09b803f
phdye <phdye@acm.org>
2024-03-08 01:47:09 +0000

    3/7/24

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -3,21 +3,29 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut s = String::from(input);
+    s.push_str(" world!");
+    s
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let mut input = String::from(input);
+    let needle = "cars".to_string();
+    let needle_offset = input.find(&needle).unwrap_or(input.len());
+    if needle_offset < input.len() {
+        let needle_span = needle_offset + needle.len();
+        // Replace the range up until the 'needle' from the input string
+        input.replace_range(needle_offset..needle_span, "balloons");
+    }
+    input
 }
 
 #[cfg(test)]
